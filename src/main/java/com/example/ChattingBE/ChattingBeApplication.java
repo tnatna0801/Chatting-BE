ff2714e38b24dce1feae61537a6a48a0e9a07ae5
tnatna0801 <min120926@gmail.com>
2023-12-29 22:50:15 +0900

    project setting

---
+++ src/main/java/com/example/ChattingBE/ChattingBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChattingBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ChattingBE/ChattingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChattingBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
